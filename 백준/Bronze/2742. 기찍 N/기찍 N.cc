0afe2e0dc3496795f8f0cba0b17ee8c47afdb758
Jeong-Minkyeong <72567631+Jeong-Minkyeong@users.noreply.github.com>
2022-10-12 23:38:02 +0900

    [Bronze IV] Title: 기찍 N, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+	int num = 0;
+	int size;
+
+	cin >> num;
+
+	size = num;
+
+	for (int i =0; i < size; i++) {
+
+		cout << num << "\n";
+		num--;
+
+	}
+}
