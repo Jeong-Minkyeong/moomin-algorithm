fa0401500a3d4a197eb10bbd7015ca72c40821e0
Jeong-Minkyeong <72567631+Jeong-Minkyeong@users.noreply.github.com>
2022-10-20 19:06:13 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int a,b;
+    int temp;
+    
+    cin>> a;
+    cin>> b;
+    
+    temp = a*b;
+    
+    cout<< temp;
+    
+}
