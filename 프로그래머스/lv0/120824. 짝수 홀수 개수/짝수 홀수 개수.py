736ce875a5cc14167a9ab21483eff149ffe8e93c
Jeong-Minkyeong <72567631+Jeong-Minkyeong@users.noreply.github.com>
2023-03-09 00:45:01 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -1,3 +1,9 @@
+'''def solution(num_list):
+    answer = [0,0]
+    for n in num_list:
+        answer[n%2]+=1
+    return answer'''
+
 def solution(num_list):
     answer = []
     answer1 = [i for i in num_list if i%2 == 0]
