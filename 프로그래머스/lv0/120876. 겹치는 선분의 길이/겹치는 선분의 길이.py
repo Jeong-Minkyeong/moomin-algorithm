6314a5bfb7d0b0562c6b21f5eaff5d5787258227
Jeong-Minkyeong <72567631+Jeong-Minkyeong@users.noreply.github.com>
2023-04-12 14:39:19 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.01 ms, Memory: 9.99 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
@@ -1,27 +1,9 @@
+
 def solution(lines):
-    overlaps = []  # 겹치는 부분을 저장할 리스트
-    for i in range(len(lines)):
-        for j in range(i+1, len(lines)):
-            if lines[i][1] >= lines[j][0] and lines[i][0] <= lines[j][1]:
-                # 두 선분이 겹치는 경우
-                overlap_start = max(lines[i][0], lines[j][0])
-                overlap_end = min(lines[i][1], lines[j][1])
-                overlap_length = overlap_end - overlap_start
-
-                # 중복 계산 방지 및 중첩되는 부분 제거
-                for start, end in overlaps:
-                    if overlap_start <= end and overlap_end >= start:
-                        overlap_start = min(overlap_start, start)
-                        overlap_end = max(overlap_end, end)
-                        overlap_length = overlap_end - overlap_start
-                        overlaps.remove((start, end))
-
-                overlaps.append((overlap_start, overlap_end))
-
-    # 겹치는 부분들의 길이 합산
-    total_length = sum([end - start for start, end in overlaps])
-
-    return total_length
+    s1 = set(i for i in range(lines[0][0], lines[0][1]))
+    s2 = set(i for i in range(lines[1][0], lines[1][1]))
+    s3 = set(i for i in range(lines[2][0], lines[2][1]))
+    return len((s1 & s2) | (s2 & s3) | (s1 & s3))
                              
 # [-2 3] [-1 0] [0 1]
 # [[0, 5] [1, 10] [3, 9]]
