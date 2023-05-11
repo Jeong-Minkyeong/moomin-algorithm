d13155819d02f9772c13c26d170781fd5c41e53c
Jeong-Minkyeong <72567631+Jeong-Minkyeong@users.noreply.github.com>
2023-05-12 00:51:49 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+def solution(n):
+    
+    answer = []
+    result = []
+    
+    while(n>0):
+        n , i = divmod(n, 3)
+        answer.append(i)
+        
+    for i,v in enumerate(answer):
+        result.append(v*(3**(len(answer)-i-1)))
+    
+    return sum(result)
+
