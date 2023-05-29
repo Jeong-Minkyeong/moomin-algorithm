5ca07d0d8dee74af2af91bb198ab220dd994ce63
Jeong-Minkyeong <72567631+Jeong-Minkyeong@users.noreply.github.com>
2023-05-30 00:57:41 +0900

    [level 2] Title: 숫자의 표현, Time: 0.36 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,13 +1,3 @@
 def solution(n):
-    available = []
-    sqrt_n = int((n * 2) ** 0.5)
-    for i in range(sqrt_n):
-        if (2 * n / (i + 1)) % 1 == 0:
-            available.append(i + 1)
-            available.append(int(2 * n / (i + 1)))
-    total = 0
-    for i in available:
-        if (n * 2) / i + 1 - i > 0 and ((n * 2) / i + 1 - i) % 2 == 0:
-            total += 1
-    return total
-        
+    return len([i  for i in range(1,n+1,2) if n % i is 0])
+      
