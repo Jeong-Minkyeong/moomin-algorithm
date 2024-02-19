f39b200625ef86c340dc181f06611566c2173ba4
Jeong-Minkyeong <72567631+Jeong-Minkyeong@users.noreply.github.com>
2024-02-19 21:49:35 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -0,0 +1,9 @@
+def solution(brown, yellow):
+    answer = []
+    temp = [ [i, yellow//i ] for i in range(yellow+1,0,-1) if yellow % i == 0]
+    temp = [i for i in temp if i[0] >= i[1]]
+    for i in temp:
+        tile = 4 + i[0]*2 +i[1]*2
+        if tile == brown:
+            return [i[0]+2, i[1]+2]
+    #return temp
