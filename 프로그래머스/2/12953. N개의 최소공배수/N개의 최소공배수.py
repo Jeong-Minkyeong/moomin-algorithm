e59d4f7b414a0eea5acf2150c43c57e5892129e4
Jeong-Minkyeong <72567631+Jeong-Minkyeong@users.noreply.github.com>
2024-03-26 17:25:48 +0900

    [level 2] Title: N개의 최소공배수, Time: 929.90 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -7,14 +7,13 @@ def solution(arr):
             for k in range(v, (v * arr[i+1]) + 1):
                     if k % v == 0 and k % arr[i+1] == 0:
                         answer = k
-                        print(k, i)
                         break
         elif i > 1:
             for k in range(min(answer, v), (answer * v) + 1):
                 if k % answer == 0 and k % v == 0:
                     answer = k
-                    print(k, i)
                     break
     return answer
+
 # (2,6) 8 14
 # (2 6 8) 14
