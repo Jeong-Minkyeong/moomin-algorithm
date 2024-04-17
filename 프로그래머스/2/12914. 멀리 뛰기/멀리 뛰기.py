f1a23b33bd588592ab3f94aae333bf637bdaeae0
Jeong-Minkyeong <72567631+Jeong-Minkyeong@users.noreply.github.com>
2024-04-17 15:39:54 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.22 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -0,0 +1,14 @@
+def solution(n):
+    answer = []
+    a,b = 1,2
+    if n == 1:
+        return 1
+    
+    elif n == 2:
+        return 2
+    
+    for i in range(1,n):
+        a,b = b, a+b
+    return a%1234567
+
+
