b1342d17537ef7c291958dca1d4173343bac7dbf
Jeong-Minkyeong <72567631+Jeong-Minkyeong@users.noreply.github.com>
2025-02-18 14:29:34 +0900

    [Silver I] Title: 절댓값 힙, Time: 292 ms, Memory: 25996 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,43 +1,38 @@
-import java.util.PriorityQueue;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-public class Main { // 우선순위 큐
+public class Main {
 
-    public static void main(String[] args){
-
-        // 입력
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-
-        // 우선순위 큐 절댓값 기준으로 정렬되도록 설정하기
-        // 절댓값이 같으면 음수 우선 설정하기
-        PriorityQueue<Integer> pQ = new PriorityQueue<>((o1,o2) -> {
-            int first_abs = Math.abs(o1);
-            int second_abs = Math.abs(o2);
-
-            if(first_abs == second_abs){   // 절댓값이 같은 경우 음수 우선
-                return o1 > o2 ? 1 : -1; // 양수 음수
-            }
-            return first_abs - second_abs; // 절댓값 작은 데이터 우선
-        });
-
-
-        // 입력이 0인경우와 아닌경우
-        for(int i = 0; i < N; i++){
-            int num = sc.nextInt();
-
-            if (num == 0) {
-                // 배열이 비었을 경우
-                if(pQ.isEmpty()){
-                    System.out.println("0");
-                } else {
-                    // 배열에서 가장 절댓값이 작은 값을 출력
-                    System.out.println(pQ.poll());
-                }
-            } else {
-                // 배열에 값을 삽입
-                pQ.add(num);
-            }
+    static int N;
+    static StringBuilder sb = new StringBuilder();
+    // 우선순위 큐 정렬기준 다시 정의
+    static PriorityQueue<Integer> priorityQ = new PriorityQueue<>((o1, o2) ->
+    {
+        if(Math.abs(o1) == Math.abs(o2)){ // 같으면 음수 우선
+            return o1 - o2;
         }
+        return Math.abs(o1) - Math.abs(o2); // 다르면 절댓값 기준 작은 것
+    });
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i<N; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num != 0) {
+                priorityQ.add(num);
+                continue;
+            }
+            if (priorityQ.isEmpty() && num == 0) {
+                sb.append(0).append('\n');
+                continue;
+            }
+            sb.append(priorityQ.poll()).append('\n');
+        }
+
+        System.out.println(sb.toString().trim());
     }
 }
