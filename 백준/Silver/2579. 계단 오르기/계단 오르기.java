fa28365102426c2e57b2ca36313436d13e92e267
Jeong-Minkyeong <72567631+Jeong-Minkyeong@users.noreply.github.com>
2025-03-12 17:41:43 +0900

    [Silver III] Title: 계단 오르기, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -4,42 +4,41 @@ import java.util.*;
 public class Main {
 
     static int N;
+    static int score;
     static int[] arr;
     static int[] memo;
-    static List<Integer> ans =  new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
 
-        // 계단의 수 입력받기
+        // N 입력받기(계단 수)
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
 
         // 계단 입력받기
-        arr = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+        arr = new int[N+1];
+        arr[0] = 0;
+        for(int i = 1; i<=N; i++){
+            st = new StringTokenizer(br.readLine());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 탐색 시작 - 처음이후 한계단 밟았을때
-        memo = new int[N + 1];
+        // 계단 올라가기
+        memo = new int[N+1];
 
-        memo[1] = arr[1];
-
-        if (N >= 2) {
-            memo[2] = arr[1] + arr[2];
+        // 3층부터 최댓값 찾기
+        // 현재까지 최대 = max(나의 전전, 나의 전+나의전전전) + 나
+        for(int i = 0; i<arr.length; i++){
+            // 2층까지는 최적값 세팅
+            if(i < 3){
+                memo[i] = arr[i] + score;
+                score += arr[i];
+                continue;
+            }
+            score = Math.max(memo[i-2], (arr[i-1] + memo[i-3])) + arr[i];
+            memo[i] = score;
         }
-
-        dp();
-        System.out.println(memo[N]);
-        ans.add(memo[N - 1]);
-    }
-
-    public static void dp(){
-        for(int i = 3; i<=N; i++){
-            // 점화식을 3부터 수행함
-            // memo[3] = arr[3] + (memo[1] vs memo[0] + arr[2])
-            // memo[4] = arr[4] + (memo[2] vs memo[1] + arr[3])
-            memo[i] = Math.max(memo[i-2], memo[i-3] + arr[i-1]) + arr[i];
-        }
+        System.out.println(score);
     }
 }
