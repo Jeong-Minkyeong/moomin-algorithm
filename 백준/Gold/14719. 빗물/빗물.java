726ac1c31ce5861e27f21f51bc7731b62ba52b46
Jeong-Minkyeong <72567631+Jeong-Minkyeong@users.noreply.github.com>
2025-04-07 13:59:55 +0900

    [Gold V] Title: 빗물, Time: 116 ms, Memory: 14916 KB -BaekjoonHub

---
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int H, W;
+    static int[][] arr;
+    static int ans;
+    public static void main(String[] args) throws IOException {
+
+        // H, W 입력받기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        H = Integer.parseInt(st.nextToken());
+        W = Integer.parseInt(st.nextToken());
+
+        // 블록 입력받기
+        arr = new int[H][W+1];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 1; i<=W; i++){
+            int h = Integer.parseInt(st.nextToken());
+            for(int j = 0; j<h; j++){
+                arr[H-j-1][i] = 1;
+            }
+        }
+
+        // 빗물 탐색
+        for(int i = H-1; i>=0; i--){
+            int start = 0;
+            int end = 1;
+            while(end <= W){
+                if(arr[i][end] == 1 && start == 0){
+                    start = end;
+                    end++;
+                    continue;
+                }
+
+                if(arr[i][end] == 1 && start != 0){
+                    ans += end - start - 1;
+                    start = end;
+                    end++;
+                    continue;
+                }
+                end++;
+            }
+        }
+        System.out.println(ans);
+    }
+}
