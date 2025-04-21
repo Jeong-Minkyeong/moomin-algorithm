f7a4d1dbd348c5cbbea2d54bd2e83aa682c94503
Jeong-Minkyeong <72567631+Jeong-Minkyeong@users.noreply.github.com>
2025-04-21 23:31:47 +0900

    [Bronze V] Title: 홍익대학교, Time: 164 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+   
+    public static void main(String[] args) throws IOException {
+
+        // N 입력받기
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        
+        System.out.println(N-1946);
+    }
+}
