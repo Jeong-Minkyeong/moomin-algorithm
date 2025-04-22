e6e4e2779d3113ae12f168596f7981863e2fa1b5
Jeong-Minkyeong <72567631+Jeong-Minkyeong@users.noreply.github.com>
2025-04-22 23:51:06 +0900

    [Bronze V] Title: SciComLove, Time: 100 ms, Memory: 13996 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    
+    public static void main(String[] args) throws IOException {
+
+        // N 입력받기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        
+        for(int i = 0; i<N; i++){
+            System.out.println("SciComLove");
+        }
+    }
+}
