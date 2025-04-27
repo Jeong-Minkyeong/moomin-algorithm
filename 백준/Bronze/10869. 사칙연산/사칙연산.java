d04606237f62a0072ce22b528aafef96ff1d33e4
Jeong-Minkyeong <72567631+Jeong-Minkyeong@users.noreply.github.com>
2025-04-27 23:21:13 +0900

    [Bronze V] Title: 사칙연산, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        
+        System.out.println(N+M);
+        System.out.println(N-M);
+        System.out.println(N*M);
+        System.out.println(N/M);
+        System.out.println(N%M);
+    }
+}
